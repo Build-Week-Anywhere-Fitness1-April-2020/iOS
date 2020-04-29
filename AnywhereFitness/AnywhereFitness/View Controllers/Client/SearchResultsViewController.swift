9bac9eb4c864ec52f83c45ebcd16c296aa7d3366
Christopher DeVito <christopher.devito@protonmail.com>
2020-04-29 16:15:32 -0400

    Simplified search flow, created view controller files for search VCs.

---
+++ AnywhereFitness/AnywhereFitness/View Controllers/Client/SearchResultsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchResultsViewController.swift
+//  AnywhereFitness
+//
+//  Created by Christopher Devito on 4/29/20.
+//  Copyright © 2020 Christopher Devito. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
