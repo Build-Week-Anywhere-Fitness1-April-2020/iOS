71d5638b782a3260cbe6df8f6f76bea8a066196e
Christopher DeVito <christopher.devito@protonmail.com>
2020-04-29 00:30:34 -0400

    Changed courseRepresentation.days to array. Began implementing Page3CreateCourseViewController.

--- AnywhereFitness/AnywhereFitness/View Controllers/Instructor/Page3CreateCourseViewController.swift
+++ AnywhereFitness/AnywhereFitness/View Controllers/Instructor/Page3CreateCourseViewController.swift
@@ -13,13 +13,42 @@ class Page3CreateCourseViewController: UIViewController {
     // MARK: - Properties
     var course: CourseRepresentation?
     var courseController: CourseController?
+    var sunday: Bool = false
+    lazy var days: [Bool] = {
+        var temp: [Bool] = [sunday]
+        return temp
+    }()
+    lazy var dayButtons: [UIButton] = {
+        var temp: [UIButton] = [sundayButton]
+        return temp
+    }()
     
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var classCost: UITextField!
+    @IBOutlet weak var classStartTime: UITextField!
+    @IBOutlet weak var classEndTime: UILabel!
+    @IBOutlet var sundayButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
+
+    // MARK: - IBAction
+    @IBAction func sundayButtonTapped(_ sender: Any) {
+        sunday.toggle()
+        sundayButton.backgroundColor = sunday ? .systemOrange : .systemGray
+    }
+
+    @IBAction func nextButtonTapped(_ sender: Any) {
+        for index in 0..<dayButtons.count where days[index] {
+            guard let day = dayButtons[index].titleLabel?.text else { return }
+            course?.days.append(day)
+        }
+    }
+
     // MARK: - Navigation
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
