74615130a6abe5fe3a523a7c8d18f991619fbe81
Christopher DeVito <christopher.devito@protonmail.com>
2020-04-28 23:35:15 -0400

    Implemented Page2CreateCourseViewController. Added remaining view controllers for course creation.

---
+++ AnywhereFitness/AnywhereFitness/View Controllers/Instructor/Page3CreateCourseViewController.swift
@@ -0,0 +1,31 @@
+//
+//  Page3CreateCourseViewController.swift
+//  AnywhereFitness
+//
+//  Created by Christopher Devito on 4/28/20.
+//  Copyright © 2020 Christopher Devito. All rights reserved.
+//
+
+import UIKit
+
+class Page3CreateCourseViewController: UIViewController {
+    
+    // MARK: - Properties
+    var course: CourseRepresentation?
+    var courseController: CourseController?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+       guard let page3VC = segue.destination as? Page3CreateCourseViewController else { return }
+       page3VC.course = course
+       page3VC.courseController = courseController
+    }
+
+}
